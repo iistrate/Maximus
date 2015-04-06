f8294a6b94f3f524d00455233cb66a9255456150
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2015-04-06 19:23:36 -0400

    Background image, ground tile added, player will not fall when on floor, jump animation ends on floor, jump = jump factor (needs smoothed out)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,33 +2,38 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
 	Rigidbody2D rigidBody;
 	Animator animator;
 	bool jumping = false;
 	bool flipped = false;
 	bool flip = false;
+	string animation = "Player_Idle";
 
 	public int speed;
+	public int jumpFactor;
 
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody2D> ();	
 		animator = GetComponent<Animator> ();
 	}
-	
+	// Physics update
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
-		rigidBody.velocity = movement * speed;
 
 		if (Input.GetKey ("up") && jumping == false) {
+			movement.y = (float)jumpFactor;
 			jumping = true;
+		} 
+		else {
+			movement.y = 0;
 		}
+		rigidBody.velocity = movement * speed;
 
 		if (jumping) {
-			animator.CrossFade ("Player_Jumping", 0.0f);
+			animation = "Player_Jumping";
 		}
 		else if (Input.GetKey ("right") || Input.GetKey ("left")) {
 			if ((flipped && Input.GetKey ("right")) || (!flipped && Input.GetKey("left"))) {
@@ -41,10 +46,17 @@ public class PlayerController : MonoBehaviour {
 				flip = false;
 				flipped = flipped == true ? false : true;
 			}
-			animator.CrossFade ("Player_Walking", 0.0f);
+			animation = "Player_Walking";
 		} 
 		else {
-			animator.CrossFade ("Player_Idle", 0.0f);
+			animation = "Player_Idle";
+		}
+		animator.CrossFade (animation, 0.0f);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Ground") {
+			jumping = false;
 		}
 	}
 }
