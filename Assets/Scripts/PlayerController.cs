f8d053d6ad8c0bf2256a032376e9f26e31bbd4d2
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2015-04-06 17:40:30 -0400

    jump animation cut and added, flip logic added, camera follows player, gravity on

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ public class PlayerController : MonoBehaviour {
 
 	Rigidbody2D rigidBody;
 	Animator animator;
+	bool jumping = false;
+	bool flipped = false;
+	bool flip = false;
+
 	public int speed;
 
 	// Use this for initialization
@@ -18,7 +22,25 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
 		rigidBody.velocity = movement * speed;
-		if (Input.GetKey ("up") || Input.GetKey ("down") || Input.GetKey ("right") || Input.GetKey ("left")) {
+
+		if (Input.GetKey ("up") && jumping == false) {
+			jumping = true;
+		}
+
+		if (jumping) {
+			animator.CrossFade ("Player_Jumping", 0.0f);
+		}
+		else if (Input.GetKey ("right") || Input.GetKey ("left")) {
+			if ((flipped && Input.GetKey ("right")) || (!flipped && Input.GetKey("left"))) {
+				flip = true;
+			}
+			if (flip) {
+				Vector3 scale = transform.localScale;
+				scale.x *= -1;
+				transform.localScale = scale;
+				flip = false;
+				flipped = flipped == true ? false : true;
+			}
 			animator.CrossFade ("Player_Walking", 0.0f);
 		} 
 		else {
