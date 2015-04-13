efd51574606f5d8257ad606ab4be9bee8323de68
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2015-04-12 21:30:04 -0400

    added explosions, and sound to tank weapon

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -5,6 +5,7 @@ public class DestroyByContact : MonoBehaviour {
 
 	public int scoreValue;
 	private GameController gameController;
+	public GameObject explosion;
 
 	void Start() {
 		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
@@ -13,10 +14,13 @@ public class DestroyByContact : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.tag == "Bullet") {
+			explosion.GetComponent<Animator>().StartPlayback();
+			Instantiate(explosion, transform.position, transform.rotation);
 			if (this.gameObject.tag == "Player") {
 				gameController.gameOver();
 			}
 			Destroy(this.gameObject);
+			Destroy(gameObject);
 			gameController.addScore (scoreValue);
 		}
 	}}
